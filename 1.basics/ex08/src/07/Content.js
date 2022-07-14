d94ca10ab2b78d7ac80bde7e272179c23a57c5df
Aldebalan <tjdgus4093@naver.com>
2022-07-14 17:19:00 +0900

    JSX Tutorial #5

--- 1.basics/ex08/src/07/Content.js
+++ 1.basics/ex08/src/07/Content.js
@@ -1,14 +1,12 @@
 import React, { Component, Fragment } from 'react';
-import Clock01 from './Clock01';
-import Clock02 from './Clock02';
+import Clock from './Clock';
 
 class Content extends Component {
     render() {
         return (
             <Fragment>
-                <p>{'특징3: JSX 표현식 표기법({js 표현식})'}</p>
-                <Clock01 />
-                <Clock02 />
+                <p>Dynamic HTML Rendering</p>
+                <Clock />
             </Fragment>
         );
     }
