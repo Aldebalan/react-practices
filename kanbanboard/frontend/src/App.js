bc1aa47f9116a8fd3f442ede367545574c085a62
Aldebalan <tjdgus4093@naver.com>
2022-07-20 10:34:59 +0900

    kanbanboard #1

---
+++ kanbanboard/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import KanbanBoard from './KanbanBoard'
+
+const App = () => {
+  return (
+    <div id={'App'}>
+      <KanbanBoard/>
+    </div>
+  )
+}
+
+export default App
