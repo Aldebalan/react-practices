da502448411c720d240eb2291512bf8821033348
Aldebalan <tjdgus4093@naver.com>
2022-07-20 11:55:05 +0900

    제어 컴포넌트

--- 2.component/ex03/src/02/TitleBar01.js
+++ 2.component/ex03/src/02/TitleBar01.js
@@ -3,9 +3,20 @@ import React, {Component} from 'react';
 export default class TitleBar01 extends Component {
     constructor(props) {
         super(props);
+
+        // this.no VS this.state.no
         this.no = 10;
+        this.state = {
+            no: 10
+        };
     }
     onclickHandler() {
+        // this.no VS this.state.no
+        this.no++;
+        this.setState({
+            no: this.state.no + 1
+        })
+
         console.log('TitleBar01 clicked~!' + this.no)
     }
 
--- 2.component/ex03/src/02/TitleBar02.js
+++ 2.component/ex03/src/02/TitleBar02.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function TitleBar02() {
-    const onclickHandler = e => {
-        console.log('TitleBar02 Clicked~!!')
-    }
+    const [stateNo, setStateNo] = useState(10);
 
     return (
         <h1
-            onClick={onclickHandler}
+            onClick={e => {
+                setStateNo(stateNo + 1);
+                console.log('TitleBar02 Clicked:' + stateNo);
+            }}
             style={{cursor: 'pointer'}}>
                 Function Handler(Functional Component)
+        <br/>
+        {
+            stateNo
+        }
         </h1>
     )
 }
