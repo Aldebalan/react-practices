fd80cd346db6e23130ac267ecc2b41dc4c55b63c
Aldebalan <github tjdgus4093@naver.com>
2022-07-26 10:33:31 +0900

    7.26 #1

--- 2.component/ex05/src/03/App.js
+++ 2.component/ex05/src/03/App.js
@@ -3,16 +3,35 @@ import './assets/scss/App.scss'
 import Clock from './Clock';
 
 export default class App extends Component {
-    constructor() {
-        super(...arguments);
+    constructor(props) {
+        super(props);
+        this.state = {
+            hours: 12,
+            minutes: 12,
+            seconds: 30,
+            session: 'am'
+        }
     }
 
     render() {
         return (
             <div className='clock-display'>
-                <h2>ex05 - Component LifeCycle Practice</h2>
-                    <Clock />
+                <h2>ex05 - Class Component LifeCycle Practice</h2>
+                    <Clock hours={this.state.hours} minutes={this.state.minutes} seconds={this.state.seconds} session={'pm'}/>
             </div>
         );
+    }
+
+    componentDidMount() {
+        setInterval(function() {
+            const now = new Date();
+            const hours = now.getHours();
+            this.setState({
+                hours: ('0' + (hours > 12 ? hours - 12 : hours)).slice(-2),
+                minutes: ('0' + now.getMinutes()).slice(-2),
+                seconds: ('0' + now.getSeconds()).slice(-2),
+                session: hours > 12 ? 'PM' : 'AM'
+            });
+        }.bind(this),1000);
     }
 }
